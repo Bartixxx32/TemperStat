7917dd9afabf53921c651d6ecff2e7f2fc7c174d
Aditya Borgaonkar <borg.aditya@gmail.com>
2020-03-17 18:58:33 +0530

    Major Changes

--- app.py
+++ app.py
@@ -25,16 +25,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['POST', 'GET'])
-def index():
-    temperature, humidity = sensor_1()
-    with open('readings.csv', 'a') as file:
-    	writer = csv.writer(file)
-    	writer.writerow([temperature, humidity])
-
+@app.route('/mail', methods = ['POST', 'GET'])
+def mail():
 	# sgothoskar967@gmail.com
 	# borg.aditya@gmail.com
-	'''
 	SUBJECT = 'IOT Project :: TemperStat Readings'
 	FILENAME = 'readings.csv'
 	FILEPATH = 'readings.csv'
@@ -43,27 +37,30 @@ def index():
 	TO_EMAIL = request.form['email']
 	SMTP_SERVER = 'smtp.gmail.com'
 	SMTP_PORT = 587
-
 	msg = MIMEMultipart()
 	msg['From'] = MY_EMAIL
 	msg['To'] = COMMASPACE.join([TO_EMAIL])
 	msg['Subject'] = SUBJECT
-
 	part = MIMEBase('application', "octet-stream")
 	part.set_payload(open(FILEPATH, "rb").read())
 	encoders.encode_base64(part)
 	part.add_header('Content-Disposition', 'attachment', filename=FILENAME)
 	msg.attach(part)
-
 	smtpObj = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
 	smtpObj.ehlo()
 	smtpObj.starttls()
 	smtpObj.login(MY_EMAIL, MY_PASSWORD)
 	smtpObj.sendmail(MY_EMAIL, TO_EMAIL, msg.as_string())
 	smtpObj.quit()
-	'''
-    return render_template("index.html",temperature=temperature, humidity=humidity)
+	return render_template("mail.html")
 
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    temperature, humidity = sensor_1()
+    with open('readings.csv', 'a') as file:
+    	writer = csv.writer(file)
+    	writer.writerow([temperature, humidity])
+    return render_template("index.html",temperature=temperature, humidity=humidity)
 
 
 def sensor_1():
@@ -75,48 +72,6 @@ def sensor_1():
 if __name__ == "__main__":
     app.run(debug=True)
 
-'''
-time, temperature, humidity = "12:45","30 Deg Cel","45%"
-
-with open('readings.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Time", "Temperature", "Humidity"])
-    writer.writerow([time, temperature, humidity])
-    writer.writerow(["12:46", "32 Deg Cel", "51 %"])
-    writer.writerow(["12:47", "33 Deg Cel", "65 %"])
-
-
-# sgothoskar967@gmail.com
-# borg.aditya@gmail.com
-
-SUBJECT = 'IOT Project :: TemperStat Readings'
-FILENAME = 'readings.csv'
-FILEPATH = 'readings.csv'
-MY_EMAIL = 'temperstat@gmail.com'
-MY_PASSWORD = input()
-TO_EMAIL = 'ksanket1998@gmail.com'
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
-
-msg = MIMEMultipart()
-msg['From'] = MY_EMAIL
-msg['To'] = COMMASPACE.join([TO_EMAIL])
-msg['Subject'] = SUBJECT
-
-part = MIMEBase('application', "octet-stream")
-part.set_payload(open(FILEPATH, "rb").read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', 'attachment', filename=FILENAME)
-msg.attach(part)
-
-smtpObj = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login(MY_EMAIL, MY_PASSWORD)
-smtpObj.sendmail(MY_EMAIL, TO_EMAIL, msg.as_string())
-smtpObj.quit()
-
-'''
 
 
 
