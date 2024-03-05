28281731885eab9bb9b2968b0ae5b1e6097f67e8
Bartixxx32 <mcbplay1@gmail.com>
2024-03-05 17:27:28 +0100

    test

--- app.py
+++ app.py
@@ -7,24 +7,25 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE
 from email import encoders
 from datetime import datetime
-from flask import render_template, Flask, request
+from flask import render_template, request
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 
 pin = 4
 sensor = Adafruit_DHT.DHT11
 
+from flask import Flask
+
 app = Flask(__name__)
 
 data_temp = []
 data_hum = []
 data_time = []
-last_log_time = time.time()  # Initialize last log time
+log_interval = 1800  # Logging interval in seconds (30 minutes)
 
 
-@app.route('/mail', methods=['POST', 'GET'])
+@app.route('/mail', methods=['POST'])
 def mail():
-    # Email configuration
     SUBJECT = 'IOT Project :: TemperStat Readings'
     FILENAME = 'readings.csv'
     FILEPATH = 'readings.csv'
@@ -34,8 +35,6 @@ def mail():
     toemail = TO_EMAIL
     SMTP_SERVER = 'smtp.gmail.com'
     SMTP_PORT = 587
-    
-    # Compose email
     msg = MIMEMultipart()
     msg['From'] = MY_EMAIL
     msg['To'] = COMMASPACE.join([TO_EMAIL])
@@ -45,8 +44,6 @@ def mail():
     encoders.encode_base64(part)
     part.add_header('Content-Disposition', 'attachment', filename=FILENAME)
     msg.attach(part)
-    
-    # Send email
     smtpObj = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     smtpObj.ehlo()
     smtpObj.starttls()
@@ -56,40 +53,37 @@ def mail():
     return render_template("mail.html", toemail=toemail)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
-    global last_log_time  # Use the global variable for last log time
-    nowtime = datetime.now().strftime("%H:%M:%S")
     temperature, humidity = sensor_1()
+    nowtime = datetime.now().strftime("%H:%M:%S")
+
     data_temp.append(temperature)
     data_hum.append(humidity)
     data_time.append(nowtime)
+
+    if len(data_temp) >= log_interval / 5:
+        log_data()
+
     temperature_max = max(data_temp)
     humidity_max = max(data_hum)
     temperature_min = min(data_temp)
     humidity_min = min(data_hum)
-    
-    # Check if 30 minutes have passed since the last log
-    if time.time() - last_log_time >= 1800:  # 30 minutes = 1800 seconds
-        # Log data to CSV
-        with open('readings.csv', 'a') as file:
-            writer = csv.writer(file)
-            for i in range(len(data_temp)):
-                writer.writerow([data_time[i], data_temp[i], data_hum[i]])
-        
-        # Reset data lists and update last log time
-        data_temp.clear()
-        data_hum.clear()
-        data_time.clear()
-        last_log_time = time.time()
-    render_template("index.html", temperature=temperature, humidity=humidity,
-                    data_temp=data_temp, data_time=data_time, data_hum=data_hum,
-                    temperature_max=temperature_max, humidity_max=humidity_max,
-                    temperature_min=temperature_min, humidity_min=humidity_min)
-    
-    return render_template("index.html", temperature=temperature, humidity=humidity,
-                           temperature_max=temperature_max, humidity_max=humidity_max,
-                           temperature_min=temperature_min, humidity_min=humidity_min)
+
+    return render_template("index.html", temperature=temperature, humidity=humidity, data_temp=data_temp,
+                           data_time=data_time, data_hum=data_hum, temperature_max=temperature_max,
+                           humidity_max=humidity_max, temperature_min=temperature_min, humidity_min=humidity_min)
+
+
+def log_data():
+    with open('readings.csv', 'a') as file:
+        writer = csv.writer(file)
+        for i in range(len(data_temp)):
+            writer.writerow([data_time[i], data_temp[i], data_hum[i]])
+    # Clear data lists after logging
+    data_temp.clear()
+    data_hum.clear()
+    data_time.clear()
 
 
 def sensor_1():
